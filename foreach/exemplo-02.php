f46704d2f606d495a91d0b3b7a1e065c398fbeef
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-02-26 14:50:21 -0300

    Foreach

---
+++ foreach/exemplo-02.php
@@ -0,0 +1,16 @@
+<form class="" action="" method="get">
+
+  <input type="text" name="nome">
+  <input type="date" name="nascimento">
+  <input type="submit" value="OK">
+
+</form>
+<?php
+  if (isset($_GET)) {
+    foreach ($_GET as $key => $value) {
+      echo "nome do campo: ".$key.'<br>';
+      echo "Valor do campo: ".$value ;
+      echo "<hr>";
+    }
+  }
+ ?>
