b6be6fd9780335e5e5c8032dcdbfb0be8d632c31
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-02-26 11:05:51 -0300

    Loop for

---
+++ for/exemplo-02.php
@@ -0,0 +1,9 @@
+<?php
+//Loop infinito
+echo '<h1>Este é um loop infinito!!! O Apache irá para-lo!!!</h1>'
+  for ($i=0; $i < 10; $i--) {
+
+    echo $i.' - ';
+
+  }
+?>
---
+++ for/exemplo-03.php
@@ -0,0 +1,7 @@
+<?php
+
+for ($i=date('Y'); $i>date('Y')-100  ; $i--) {
+  echo $i.' - ';
+}
+
+?>
