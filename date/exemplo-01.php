b6d55a929fd7a385baa24ac99fa17e3b1c384232
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-03-26 10:44:24 -0300

    Função date() e a Função Time()

---
+++ date/exemplo-01.php
@@ -0,0 +1,7 @@
+<?php
+
+  echo date('d/m/Y H:i:s', 1521641880);
+  echo "<br>";
+  echo time();
+
+?>
---
+++ date/exemplo-02.php
@@ -0,0 +1,9 @@
+<?php
+
+  $ts = strtotime('now +1 week');
+  echo "<br>";
+  echo $ts;
+  echo "<br>";
+  echo date("l, d/m/Y", $ts);
+
+?>
