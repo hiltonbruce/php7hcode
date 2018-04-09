8e2b7b1373500e5411a1af56f8905bd4a6032e7b
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-04-09 13:40:45 -0300

    Autoload e funções SPL do PHP

---
+++ autoload/abstratas/Automovel.php
@@ -0,0 +1,31 @@
+<?php
+
+interface Veiculo
+{
+  #
+  public function acelerar($velocidade);
+  public function frenar($velocidade);
+  public function trocarMarcha($marcha);
+
+}
+
+Abstract class Automovel implements Veiculo
+{
+
+  public function acelerar($velocidade)
+  {
+    echo 'O veículo acelerou até: '. $velocidade.' Km/h';
+  }
+
+  public function frenar($velocidade)
+  {
+    echo 'O veículo frenar até: '. $velocidade.' Km/h';
+  }
+
+  public function trocarMarcha($marcha)
+  {
+    echo 'O veículo engatou a marcha '. $marcha;
+  }
+}
+
+ ?>
