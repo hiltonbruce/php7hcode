fe5cb2db4d2bdb1bc456d6e9046ffcb51a7f17b9
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-07-23 11:43:17 -0300

    Download de aquivos com file_get_contents

---
+++ Download/exemplo-01.php
@@ -0,0 +1,21 @@
+<?php
+
+  $link = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+
+  $content = file_get_contents($link);
+
+  $parse = parse_url($link);
+
+  $basename = (basename($parse['path']));
+
+  $file = fopen ($basename,'w+');
+
+  fwrite($file, $content);
+
+  fclose($file);
+
+
+  // var_dump (basename($parse['path']));
+
+ ?>
+<img src="<?=$basename?>">
